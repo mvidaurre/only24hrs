d38f17a48c7c2d366de199336d99b27c31bc3168
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2012-06-19 11:27:25 -0500

    Initial install of Devise

--- Gemfile
+++ Gemfile
@@ -20,7 +20,13 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'capybara'
+end
+
 gem 'jquery-rails'
+gem 'devise'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Only24hrs::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  root :to => "home#index"
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
