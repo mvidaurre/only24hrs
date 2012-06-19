3586d6985885389294b7af9f661880d196330cc5
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2012-06-19 10:53:31 -0500

    Initial commit from new Rails 3.2.5 generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Only24hrs::Application.config.secret_token = 'b4c29248f99a5d9246811fbd8c98a72eba6d555df5ecdcb8bcbe39822d4597b108bb93eda3565d203b79a2cf6fa696c4577aedc6b08b283b881e7430937917e4'
