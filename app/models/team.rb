b4346e6edcb976d6d950256ed4de3787b22dccfa
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2012-06-25 05:17:06 -0500

    Adapting forms in new models to web site style

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ActiveRecord::Base
   attr_accessible :goal, :name, :user
   has_many :actors, :as => :subject
+  belongs_to :leader, :class_name => 'User', :foreign_key => "user_id"
+  belongs_to :main_goal, :class_name => 'Goal', :foreign_key => "goal_id"
 end
