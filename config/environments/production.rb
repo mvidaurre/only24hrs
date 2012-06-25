7bff8104d3358c0099237c054366cd6a90f617c5
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2012-06-25 15:09:34 -0500

    Revoking the: Putting HighCarts in the precompile for the assets pipeline in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Only24hrs::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.precompile += %w( polyfills.js  highcahrts.js)
+  config.assets.precompile += %w( polyfills.js )
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
