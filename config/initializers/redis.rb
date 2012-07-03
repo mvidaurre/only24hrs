2330f5be1d73e0c75539fe41a06acb24d4e46eb1
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2012-07-03 18:30:26 -0500

    Testing Redis in Heroku putting hardcode redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,6 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if Rails.env.production?
+  uri = URI.parse("redis://redistogo:a2ae91a3e72e9f28a216c64b68671eb7@carp.redistogo.com:9109/")
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  REDIS = Redis.new(:host => "127.0.0.1", :port => 6379)
+end
