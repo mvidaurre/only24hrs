ad370458ab5e4783a2c8b7a9ec4da533ca9d7f2d
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2012-07-03 18:10:15 -0500

    Fixing production initialization for Redis eliminating the if for development

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,2 @@
-if Rails.env.development?
-  REDIS = Redis.new(:host => "127.0.0.1", :port => 6379)
-else
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
