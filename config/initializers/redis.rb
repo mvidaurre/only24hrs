2a8dee6776d84d32dcc346ec435779f86c380f80
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2012-07-03 18:07:11 -0500

    Fixing production initialization for Redis changing condition to verify development environment

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
-if Rails.env.production?
+if Rails.env.development?
+  REDIS = Redis.new(:host => "127.0.0.1", :port => 6379)
+else
   uri = URI.parse(ENV["REDISTOGO_URL"])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-else
-  REDIS = Redis.new(:host => "127.0.0.1", :port => 6379)
 end
