bb0d989a87e5ac4a032b6e748a73beeb451f4c45
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2012-07-03 18:02:01 -0500

    Fixing production initialization for Redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,6 @@
-REDIS = Redis.new(:host => "127.0.0.1", :port => 6379)
+if Rails.env.production?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  REDIS = Redis.new(:host => "127.0.0.1", :port => 6379)
+end
